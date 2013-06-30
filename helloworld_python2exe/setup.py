a8c94ad92a992b9c062cbb444e77c0ca68d193d1
aherrero <alexhc89@gmail.com>
2013-06-30 17:58:51 +0200

    py2exe update

--- helloworld_python2exe/setup.py
+++ helloworld_python2exe/setup.py
@@ -1,4 +1,15 @@
 from distutils.core import setup
 import py2exe
 
-setup(console=['hello.py'])
+setup(name='Aplicacion de ejemplo',
+    version='0.1',
+    description='Ejemplo del funcionamiento de distutils',
+    author='Alex',
+    author_email='Alexgmail',
+    url='https://github.com/aherrero',
+    license='GPL',
+    scripts=['hello.py'],
+    console=['hello.py'],
+    options={'py2exe': {'bundle_files': 1}},
+    zipfile=None
+)
